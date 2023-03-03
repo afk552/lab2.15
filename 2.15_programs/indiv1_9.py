ef46beea08d1982e19ffa9666b4af9aa44b696ba
afk552 <ez552@mail.ru>
2023-03-03 15:46:25 +0300

    Fix error

--- 2.15_programs/indiv1_9.py
+++ 2.15_programs/indiv1_9.py
@@ -2,11 +2,9 @@
 # -*- coding: utf-8 -*-
 
 if __name__ == "__main__":
-    vowels = ["a", "e", "i", "o", "u", "y"]
+    vowels = ("a", "e", "i", "o", "u", "y")
     with open("indiv1_text.txt", "r", encoding="utf-8") as fileptr:
         for line in fileptr.readlines():
             for word in line.split(" "):
-                if word.lower().startswith(
-                    tuple(vowels)
-                ) and word.lower().endswith(tuple(vowels)):
+                if word.lower().startswith(vowels) and word.lower().endswith(vowels):
                     print(word, end=" ")
