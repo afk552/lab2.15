06d710069d0f3bfc1e14a7e1ff8c4ec5102b1039
afk552 <ez552@mail.ru>
2023-03-03 15:59:40 +0300

    Fix format

--- 2.15_programs/os_program/audio_files_sorter.py
+++ 2.15_programs/os_program/audio_files_sorter.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
     path = os.getcwd()
     audio_ext = ["mp3", "wav", "flac", "ogg"]
     for filename in os.listdir(path):
-        file_ext = filename.split('.')[-1]
+        file_ext = filename.split(".")[-1]
         if file_ext in audio_ext:
-            artist = filename.rpartition(' - ')[0]
+            artist = filename.rpartition(" - ")[0]
             os.mkdir(artist)
             # Скопировать/переместить файл через os не получилось
-            shutil.copy(f'{filename}', f'{artist}/{filename}')
+            shutil.copy(f"{filename}", f"{artist}/{filename}")
             os.remove(f"{filename}")
