d0445455db2df5bf11eaec4c88eed2561fc5e3ed
afk552 <ez552@mail.ru>
2023-03-03 15:41:57 +0300

    Fix break

--- 2.15_programs/indiv1_9.py
+++ 2.15_programs/indiv1_9.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 if __name__ == "__main__":
-    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+    vowels = ["a", "e", "i", "o", "u", "y"]
     with open("indiv1_text.txt", "r", encoding="utf-8") as fileptr:
         for line in fileptr.readlines():
-            for word in line.split(' '):
-                if (word.lower().startswith(tuple(vowels)) and
-                        word.lower().endswith(tuple(vowels))):
-                    print(word, end=' ')
+            for word in line.split(" "):
+                if word.lower().startswith(
+                    tuple(vowels)
+                ) and word.lower().endswith(tuple(vowels)):
+                    print(word, end=" ")
