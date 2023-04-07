db1b1f58aee9d8afadc8e6b80c5a6fc212e73ff9
afk552 <ez552@mail.ru>
2023-04-07 03:33:26 +0300

    Fix opened file duration

--- 2.15_programs/indiv1_9.py
+++ 2.15_programs/indiv1_9.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 if __name__ == "__main__":
+    words = []
     vowels = ("a", "e", "i", "o", "u", "y")
     with open("indiv1_text.txt", "r", encoding="utf-8") as fileptr:
-        for line in fileptr.readlines():
-            for word in line.split(" "):
-                if word.lower().startswith(vowels) and word.lower().endswith(
-                    vowels
-                ):
-                    print(word, end=" ")
+        for line in fileptr.read().split(" "):
+            words.append(line)
+
+    for word in words:
+        if word.lower().startswith(vowels) and word.lower().endswith(vowels):
+            print(word, end=" ")
